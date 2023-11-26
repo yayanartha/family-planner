7769f40d6dbb27729af706309b8fe87ae781859f
Yayan Artha <yayanartha21@gmail.com>
2023-11-26 15:43:49 +0800

    add apps/mobile

---
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import { Link, Tabs } from 'expo-router';
+import { Pressable, useColorScheme } from 'react-native';
+
+import Colors from '../../constants/Colors';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Tab One',
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Tab Two',
+        }}
+      />
+    </Tabs>
+  );
+}
